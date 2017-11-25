48a9579f615a9b22db64fbcd98c833965e3d7f88
Hritik <hritik.verma10@gmail.com>
2017-11-25 19:16:38 +0530

    first commit milestone 1

---
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -0,0 +1,79 @@
+/* Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.engedu.anagrams;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static android.content.ContentValues.TAG;
+
+public class AnagramDictionary {
+
+    private static final int MIN_NUM_ANAGRAMS = 5;
+    private static final int DEFAULT_WORD_LENGTH = 3;
+    private static final int MAX_WORD_LENGTH = 7;
+    private Random random = new Random();
+    private ArrayList<String> wordlist = new ArrayList<>();
+
+    public AnagramDictionary(Reader reader) throws IOException {
+        BufferedReader in = new BufferedReader(reader);
+        String line;
+        while ((line = in.readLine()) != null) {
+            String word = line.trim();
+            wordlist.add(word);
+        }
+    }
+
+    public boolean isGoodWord(String word, String base) {
+        return true;
+    }
+
+    public List<String> getAnagrams(String targetWord) {
+        ArrayList<String> result = new ArrayList<String>();
+        String word = sortString(targetWord);
+        for (String temp : wordlist) {
+            if (temp.length() == targetWord.length()) {
+                if (word.equals(sortString(temp))) {
+                    result.add(temp);
+                }
+            }
+        }
+        Log.e(TAG, "getAnagrams: count " + result, null);
+        return result;
+    }
+
+    public List<String> getAnagramsWithOneMoreLetter(String word) {
+        ArrayList<String> result = new ArrayList<String>();
+        return result;
+    }
+
+    private String sortString(String input) {
+        char[] charArray = input.toLowerCase().toCharArray();
+        Arrays.sort(charArray);
+        return new String(charArray);
+    }
+
+    public String pickGoodStarterWord() {
+        return "skate";
+    }
+}
